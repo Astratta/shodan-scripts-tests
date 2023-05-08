40956e835c4d3f783c1bbd5e78e9a4c1ecabc2ac
felipe-andrade <felipe.astratta@gmail.com>
2023-05-08 15:21:04 -0300

    Finalizado, lê listas com IPs ou Netblocks

--- hostsdata-SHODAN-API.py
+++ hostsdata-SHODAN-API.py
@@ -6,6 +6,8 @@
 import os
 import json
 import requests
+import ipaddress
+from time import sleep
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -14,45 +16,55 @@ load_dotenv()
 KEY = os.getenv("API-KEY")
 baseURL = "https://api.shodan.io"
 
-'''def getDomainIp(domains): ## Método retorna uma lista de IPs recebendo uma lista de domínios como argumento 
-    endpoint = "/dns/resolve?"
-    finalURL = "{baseURL}{endpoint}hostnames={hostnames}&key={KEY}".format(baseURL=baseURL, 
-                                                                           endpoint=endpoint, 
-                                                                           hostnames=",".join([ip for ip in domains]), 
-                                                                           KEY=KEY)
+ips = []
+data = {"data": []}
+orgs = ["XXXX, YYYYY, ZZZZZ"]
 
-    try:
-        results = requests.get(finalURL)
-        results.raise_for_status()
-        return list(results.json().values())
-    except requests.exceptions.HTTPError as exception:
-        raise SystemError(exception)'''
+for o in orgs:
+    with open("IPs-"+o+".txt", "r") as file: ## Nome do arquivo deve ser IPs-ORG.txt
+        for address in file.readlines():
+            address = address.replace("\n", "")
+            if "/" in address: ## Checa se é um ip ou um netblock
+                addresses = [str(ip) for ip in ipaddress.IPv4Network(str(address))] ## Cria uma lista de IPs dado um netblock
+                for i in addresses:
+                    ips.append(i) ## Adiciona os IPs na lista geral
+            else:
+                ips.append(address) ## Adiciona o IP na lista geral
+
+    data["data"].append({"org": o})
+    data["data"][-1]["ips"] = buildData(ips, ipsData = {})
+
+## Exporta os dados em JSON
+with open("ipData.json", 'w', encoding="utf-8") as file:
+    file.write(json.dumps(data, ensure_ascii=False ,indent=4))
 
 def getIpData(ip): ## Método que retorna todas as informações de um IP
     endpoint = "/shodan/host/"
     finalURL = "{baseURL}{endpoint}{ip}?key={KEY}".format(baseURL=baseURL, endpoint=endpoint, ip=ip, KEY=KEY)
 
-    try:
-        results = requests.get(finalURL)
-        results.raise_for_status()
-        rawdata = results.json()
+    results = requests.get(finalURL)
+    rawdata = results.json()
+    if results.status_code == "200":
         datetimeObj = datetime.strptime(rawdata["last_update"], "%Y-%m-%dT%H:%M:%S.%f")
         rawdata["last_update"] = datetimeObj.strftime("%d/%m/%Y")
         return rawdata
-    except requests.exceptions.HTTPError as exception:
-        raise SystemError(exception)
+    else:
+        return rawdata
 
 def filterData(rawdata):
-    ipData = {}
-    genKeys = ["last_update", "ip_str", "hostnames", "ports", "vulns"]
+    if "error" in rawdata.keys():
+        return rawdata
+    else:
+        ipData = {}
+        genKeys = ["last_update", "ip_str", "hostnames", "ports", "vulns"]
 
-    for k in genKeys:
-        if k in rawdata.keys():
-            ipData[k]=rawdata[k]
-        else:
-            ipData[k] = None
-    
-    return filterIpServiceData(rawdata, ipData)
+        for k in genKeys:
+            if k in rawdata.keys():
+                ipData[k]=rawdata[k]
+            else:
+                ipData[k] = None
+        
+        return filterIpServiceData(rawdata, ipData)
 
 def filterIpServiceData(rawdata, ipData): ## Filtra informações de cada serviço possivelmente vulnerável por IP
     if ipData["vulns"] is None:
@@ -77,26 +89,25 @@ def buildData(ips, ipsData):
         return ipsData ## Retorna todos os dados depois que todos os IPs forem processados
     else:
         rawdata = getIpData(ips[-1]) ## Pega informação completa
+        sleep(3) ## Espera 3 segundos para não cair no rate limit da API
         ipData = filterData(rawdata) ## Filtra informações mais relevantes
-        ipsData[ipData["ip_str"]] = ipData ## Coloca as informações no dicioário
+        ipsData[ips[-1]] = ipData ## Coloca as informações no dicioário
         ips.pop() ## Deleta o último item para poder ler um novo
         return buildData(ips, ipsData) ## Processa outro dado
 
-data = {"data": []}
-orgs = ["Meta", "Google"]
+'''def getDomainIp(domains): ## Método retorna uma lista de IPs recebendo uma lista de domínios como argumento 
+    endpoint = "/dns/resolve?"
+    finalURL = "{baseURL}{endpoint}hostnames={hostnames}&key={KEY}".format(baseURL=baseURL, 
+                                                                           endpoint=endpoint, 
+                                                                           hostnames=",".join([ip for ip in domains]), 
+                                                                           KEY=KEY)
 
-for o in orgs:
-    with open("IPs-"+o+".txt", "r") as file: ## Nome do arquivo deve ser IPs-ORG.txt
-        ips = file.readlines() #Lê um arquivo com IPs, um IP por linha
-
-    data["data"].append({"org": o})
-    data["data"][-1]["ips"] = buildData(ips, ipsData = {})
-
-## Exporta os dados em JSON
-with open("ipData.json", 'w', encoding="utf-8") as file:
-    file.write(json.dumps(data, ensure_ascii=False ,indent=4))
-
-
+    try:
+        results = requests.get(finalURL)
+        results.raise_for_status()
+        return list(results.json().values())
+    except requests.exceptions.HTTPError as exception:
+        raise SystemError(exception)'''
 
 
 
